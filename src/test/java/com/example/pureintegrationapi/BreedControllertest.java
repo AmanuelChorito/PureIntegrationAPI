a5022dc6a870376e6b06bf78f7fb8ec3775affd5
amanuelchorito <amye.end@gmail.com>
2021-09-20 17:10:34 -0500

    step 1

---
+++ src/test/java/com/example/pureintegrationapi/BreedControllertest.java
@@ -0,0 +1,29 @@
+package com.example.pureintegrationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest
+public class BreedControllertest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+   void getAllList() throws Exception{
+
+        RequestBuilder requestBuilder= MockMvcRequestBuilders.get("/api/breeds//list/all")
+                .contentType(MediaType.APPLICATION_JSON);
+     mockMvc.perform(requestBuilder).andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+
+
+    }
+}
